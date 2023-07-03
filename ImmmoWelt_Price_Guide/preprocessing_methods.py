9ffb8dfd67310751ab9218f1f5bcb5de62e18cbb
FelixNeubauer <felix.neubauer@tech11.com>
2023-07-03 20:20:30 +0200

    added preprocessing draft

--- ImmmoWelt_Price_Guide/preprocessing_methods.py
+++ ImmmoWelt_Price_Guide/preprocessing_methods.py
@@ -51,3 +51,47 @@ def binarize_columns(df, column_name):
     df = pd.concat([df, df_binary], axis=1)
 
     return df
+
+def check_concat_xlsx(file1, file2, output_file):
+    # Read the Excel files
+    df1 = pd.read_excel(file1)
+    df2 = pd.read_excel(file2)
+
+    # Check if column structures match
+    if df1.columns.tolist() != df2.columns.tolist():
+        print("Column structures do not match.")
+        return
+
+    # Concatenate the data frames
+    concatenated = pd.concat([df1, df2])
+
+    # Write the concatenated data frame to a new Excel file
+    concatenated.to_excel(output_file, index=False)
+    print(f"Concatenated data saved to {output_file}")
+
+def extract_zipcode(df):
+    # Convert the "Address" column from string to dictionary
+    df['Address'] = df['Address'].apply(json.loads)
+
+    # Extract "ZipCode" from "Address" column and convert it to string
+    df['ZipCode'] = df['Address'].apply(lambda x: str(x['ZipCode']))
+
+    # Drop the old "Address" column
+    df.drop('Address', axis=1, inplace=True)
+
+    return df
+
+def rename_columns_with_umlauts(df):
+    # Replace 'ü' with 'ue' in column names
+    new_columns = df.columns.str.replace('ü', 'ue')
+
+    # Replace 'ö' with 'oe' in the modified column names
+    new_columns = new_columns.str.replace('ö', 'oe')
+
+    # Replace 'ä' with 'ae' in the modified column names
+    new_columns = new_columns.str.replace('ä', 'ae')
+
+    # Rename the columns in the DataFrame
+    df.columns = new_columns
+    return df
+
